c50cd8a92923ba01d5e0d9dd4c1dfc2a158a432c
FABIO MONTEIRO AMORIM <fmamorim_ext@grupogbi.com>
2025-04-09 13:18:17 -0300

    melhorando o codigo

--- src/main/java/br/cnpq/SbootBatchTrataXmlApplication.java
+++ src/main/java/br/cnpq/SbootBatchTrataXmlApplication.java
@@ -9,13 +9,4 @@ public class SbootBatchTrataXmlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SbootBatchTrataXmlApplication.class, args);
 	}
-
-//	@Bean
-//	public CommandLineRunner commandLineRunner(SbootBatchTrataXmlApplication sbootBatchTrataXmlApplication) {
-//		return args -> {
-//			String fileName = "meuArquivo"; // Exemplo de nome de arquivo
-//			sbootBatchTrataXmlApplication.processFile(fileName); // Chamando o método de processamento
-//		};
-//	}
-
 }
